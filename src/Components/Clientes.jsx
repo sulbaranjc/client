9e3170ae3ec325b5b6cc3994e6b959e3f1c35e1f
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-01-14 23:05:33 +0100

    crud ok

--- src/Components/Clientes.jsx
+++ src/Components/Clientes.jsx
@@ -10,12 +10,14 @@ import Form from 'react-bootstrap/Form';
 
 function Clientes() {
 const [datos,setDatos] = React.useState([])  
-//const [client_id,setClient_id] = React.useState(0)  
 const [first_name,setFirst_name] = React.useState("")  
 const [last_name,setLast_name] = React.useState("")  
 const [phone_number,setPhone_number] = React.useState("")  
 const [email,setEmail] = React.useState("")  
 const [address,setAddress] = React.useState("")  
+const [validacionModificar,setvalidacionModificar] = React.useState(false)  
+const [idModificar,setIdModificar] = React.useState(0)  
+
 
 useEffect(() =>{
   cargarDatos()
@@ -23,12 +25,14 @@ useEffect(() =>{
 
 const cargarDatos = async() => {
   const respuesta = await axios.get("http://192.168.1.219:3300/api/clients/")
+  respuesta.data.sort((a, b) => (a.client_id - b.client_id));
   setDatos(respuesta.data)
  
 }
 
 
-const addClient = async () => {
+const addClient = async (e) => {
+  e.preventDefault()
   await axios.post("http://192.168.1.219:3300/api/clients/",{
     first_name,
     last_name,
@@ -36,6 +40,7 @@ const addClient = async () => {
     email,
     address
   })
+  LimpiarFormulario()
   cargarDatos()
 }
 
@@ -44,6 +49,37 @@ const delClient = async(id) => {
   cargarDatos()
 }
 
+const activarModificacion = async(id) => {
+  const respuesta = await axios.get(`http://192.168.1.219:3300/api/clients/${id}`)
+  setFirst_name(respuesta.data[0].first_name)
+  setLast_name(respuesta.data[0].last_name)
+  setPhone_number(respuesta.data[0].phone_number)
+  setEmail(respuesta.data[0].email)
+  setAddress(respuesta.data[0].address)
+  setvalidacionModificar(true)
+  setIdModificar(id)
+}
+
+const LimpiarFormulario= () => {
+  setFirst_name("")
+  setLast_name("")
+  setPhone_number("")
+  setEmail("")
+  setAddress("")
+}
+const modificarAlumno = async(e) => {
+  e.preventDefault()
+  await axios.patch(`http://192.168.1.219:3300/api/clients/${idModificar}`,{
+    first_name,
+    last_name,
+    phone_number,
+    email,
+    address  
+  }) 
+  setvalidacionModificar(false)
+  LimpiarFormulario()
+  cargarDatos()
+}
   return (
     <Container className="w-lg" fluid={false}>
         <h1 className='text-center'>CRUD CLIENTES</h1>
@@ -74,11 +110,12 @@ const delClient = async(id) => {
             </thead>
             <tbody>
               {
+                
                 datos.map(fila => (
                   <tr key={fila.client_id}>
                     <td class="align-middle">
                       <Button className='me-centre' 
-                        variant="warning" size="sm">Edit
+                        variant="warning" size="sm" onClick={()=>activarModificacion(fila.client_id)}>Edit
                       </Button>
                     </td>
                     <td class="align-middle">
@@ -107,40 +144,46 @@ const delClient = async(id) => {
 
             <Form.Group className="mb-3" controlId="Name">
               <Form.Label>Nombre</Form.Label>
-              <Form.Control type="text" placeholder="Idique Nombres"
-              onChange={(e)=>setFirst_name(e.target.value)} />
+              <Form.Control type="text" placeholder="Indique el Nombre" 
+              onChange={(e)=>setFirst_name(e.target.value)}
+              value={first_name}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="SecondName">
               <Form.Label>Apellido  </Form.Label>
               <Form.Control type="text" placeholder="indique Apellidos"
-              onChange={(e)=>setLast_name(e.target.value)} />
+              onChange={(e)=>setLast_name(e.target.value)} 
+              value={last_name}/>
             </Form.Group>
 
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Indique el Correo"
-              onChange={(e)=>setEmail(e.target.value)} />
+              onChange={(e)=>setEmail(e.target.value)}
+              value={email}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPhone">
               <Form.Label>Telfono</Form.Label>
               <Form.Control type="tel" 
                 placeholder="Enter phone" 
-                onChange={(e)=>setPhone_number(e.target.value)} />
+                onChange={(e)=>setPhone_number(e.target.value)} 
+                value={phone_number}/>
             </Form.Group>
 
             <Form.Group  className="mb-3" controlId="formAddress">
               <Form.Label>Dirección</Form.Label>
               <Form.Control type="text" placeholder="Ingresa tu dirección"
-                onChange={(e)=>setAddress(e.target.value)} />
+                onChange={(e)=>setAddress(e.target.value)}
+                value={address}/>
             </Form.Group>
-            <Button className="text-center" 
-              variant="success" type="submit" 
-              onClick={()=>addClient()}>
-              Agregar
-            </Button>
+            {validacionModificar ? (
+              <Button className="text-center" variant="warning" type="submit" onClick={(e)=>modificarAlumno(e)}>Modificar</Button>
+            ) : (
+              <Button className="text-center" variant="success" type="submit" onClick={(e)=>addClient(e)}>Agregar</Button>
+            ) }
     </Form>
         </Col>
       </Row>   
